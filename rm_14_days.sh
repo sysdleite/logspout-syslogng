0c73a1317b119acee8bfabc5a6b58a1c395e2bcd
David Leite <david.leite.ext@oneytrust.com>
2022-03-09 14:34:26 +0100

    init project

---
+++ rm_14_days.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# to compress 2 days old logs
+#find /var/log/syslogng/ -name "*.log" -daystart -ctime +2 -type f -exec gzip {} \;
+
+# to remove 14 days old logs
+find /var/log/syslogng/ -name "*.log" -daystart -ctime +14 -type f -exec rm {} \; 
